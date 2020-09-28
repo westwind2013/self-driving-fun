dd783010ea421090a5b42ad07e38f84d074aea26
Hongbo Li <hongbo.li.cs@gmail.com>
2020-09-27 21:07:24 -0700

    Auo-formatted the code

--- src/main.cpp
+++ src/main.cpp
@@ -34,66 +34,61 @@ int main() {
   uWS::Hub h;
 
   PID pid;
-  
+
   // Initialize the pid variable.
-  pid.Init(0.1, 0, 2.0);
+  pid.Init(0.1, 0.004, 2.0);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
-                     uWS::OpCode opCode) {
-    // "42" at the start of the message means there's a websocket message event.
-    // The 4 signifies a websocket message
-    // The 2 signifies a websocket event
-    if (length && length > 2 && data[0] == '4' && data[1] == '2') {
+        uWS::OpCode opCode) {
+      // "42" at the start of the message means there's a websocket message event.
+      // The 4 signifies a websocket message
+      // The 2 signifies a websocket event
+      if (length && length > 2 && data[0] == '4' && data[1] == '2') {
       auto s = hasData(string(data).substr(0, length));
 
       if (s != "") {
-        auto j = json::parse(s);
+      auto j = json::parse(s);
 
-        string event = j[0].get<string>();
+      string event = j[0].get<string>();
 
-        if (event == "telemetry") {
-          // j[1] is the data JSON object
-          double cte = std::stod(j[1]["cte"].get<string>());
-          double speed = std::stod(j[1]["speed"].get<string>());
-          double angle = std::stod(j[1]["steering_angle"].get<string>());
-          /**
-           * TODO: Calculate steering value here, remember the steering value is
-           *   [-1, 1].
-           * NOTE: Feel free to play around with the throttle and speed.
-           *   Maybe use another PID controller to control the speed!
-           */
+      if (event == "telemetry") {
+      // j[1] is the data JSON object
+      double cte = std::stod(j[1]["cte"].get<string>());
+      double speed = std::stod(j[1]["speed"].get<string>());
+      double angle = std::stod(j[1]["steering_angle"].get<string>());
 
-          pid.UpdateError(cte);
-          const auto steer_value = -pid.TotalError();
-           
-          // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
-                    << std::endl;
+      // Calculate steering value here, remember the steering value is
+      pid.UpdateError(cte);
+      const auto steer_value = -pid.TotalError();
 
-          json msgJson;
-          msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
-          auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-          std::cout << msg << std::endl;
-          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-        }  // end "telemetry" if
+      // DEBUG
+      std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
+        << std::endl;
+
+      json msgJson;
+      msgJson["steering_angle"] = steer_value;
+      msgJson["throttle"] = 0.3;
+      auto msg = "42[\"steer\"," + msgJson.dump() + "]";
+      std::cout << msg << std::endl;
+      ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+      }  // end "telemetry" if
       } else {
         // Manual driving
         string msg = "42[\"manual\",{}]";
         ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
       }
-    }  // end websocket message if
+      }  // end websocket message if
   }); // end h.onMessage
 
   h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
-    std::cout << "Connected!!!" << std::endl;
-  });
+      std::cout << "Connected!!!" << std::endl;
+      });
 
   h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, 
-                         char *message, size_t length) {
-    ws.close();
-    std::cout << "Disconnected" << std::endl;
-  });
+        char *message, size_t length) {
+      ws.close();
+      std::cout << "Disconnected" << std::endl;
+      });
 
   int port = 4567;
   if (h.listen(port)) {
@@ -102,6 +97,6 @@ int main() {
     std::cerr << "Failed to listen to port" << std::endl;
     return -1;
   }
-  
+
   h.run();
 }
